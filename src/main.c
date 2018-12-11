b50917f02a550d9faf8de32074c69da8c5c8a885
Allan Cantin <allan.cantin@epita.fr>
2018-12-11 22:42:00 +0100

    [UPD][main.c] added the part of opening the file and convert its content with be_decode

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bencode/bencode.h"
 #include "options.h"
 
 int main(int argc, char **argv)
@@ -15,4 +18,32 @@ int main(int argc, char **argv)
     //print_options(&options);
     if (nb_options == -1)
         return 1;
+
+    FILE *torrent = fopen(options.data, "r");
+    if (torrent == NULL)
+    {
+        printf("my-bittorrent: Cannot open %s\n", options.data);
+        return 1;
+    }
+    char *buf = malloc(1024);
+    int i = 0;
+    int c;
+    while ((c = fgetc(torrent)) != EOF)
+    {
+        buf[i] = c;
+        i++;
+    }
+    buf[i] = '\0';
+    //printf("%s\n", buf);
+    struct be_node *node = be_decode(buf, strlen(buf));
+    printf("node 0 key length: %llu\n", node->element.dict[0]->key->length);
+    printf("node 0 key content: %s\n", node->element.dict[0]->key->content);
+    printf("node 0 val length: %llu\n", node->element.dict[0]->val->element.str->length);
+    printf("node 0 val content: %s\n", node->element.dict[0]->val->element.str->content);
+    printf("node 1 key length: %llu\n", node->element.dict[1]->key->length);
+    printf("node 1 key content: %s\n", node->element.dict[1]->key->content);
+    printf("node 1 val length: %llu\n", node->element.dict[1]->val->element.str->length);
+    printf("node 1 val content: %s\n", node->element.dict[1]->val->element.str->content);
+    fclose (torrent);
+    return 0;
 }
